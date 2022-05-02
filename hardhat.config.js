800193573745cbbe033f98ad93d899d8fecaa6e0
Kausik Das <kausik007007@gmail.com>
2022-05-02 23:58:10 +0530

    initial commit

---
+++ hardhat.config.js
@@ -0,0 +1,16 @@
+require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-etherscan");
+require("dotenv").config();
+
+module.exports = {
+  solidity: "0.8.1",
+  networks: {
+    rinkeby: {
+      url: `${process.env.ALCHEMY_RINKEBY_URL}`,
+      accounts: [`${process.env.RINKEBY_PRIVATE_KEY}`],
+    },
+  },
+  etherscan: {
+    apiKey: `${process.env.ETHERSCAN_API_KEY}`,
+  },
+};
---
+++ scripts/run.js
@@ -0,0 +1,29 @@
+const main = async () => {
+  const nftContractFactory = await hre.ethers.getContractFactory("MyEpicNFT");
+  const nftContract = await nftContractFactory.deploy();
+  await nftContract.deployed();
+
+  console.log("Contract deployed to:", nftContract.address);
+
+  // call the function
+  let txn = await nftContract.makeAnEpicNFT();
+  // wait for it to be mined
+  await txn.wait();
+
+  //   mint another nft for fun
+  txn = await nftContract.makeAnEpicNFT();
+  //   wait for it to be mined
+  await txn.wait();
+};
+
+const runMain = async () => {
+  try {
+    await main();
+    process.exit(0);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
+
+runMain();
